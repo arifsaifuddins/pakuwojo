d8fcf7bbe2bfdb0a90314b778df1cf31c94f5095
saifuddien <ariefsaifuddien01@gmail.com>
2021-12-26 14:37:25 +0200

    sa

--- salah/main.js
+++ salah/main.js
@@ -1,1 +1,12 @@
 console.log('ok');
+
+fetch('https://api.pray.zone/v2/times/today.json?city=khartoum')
+  .then(m => m.json())
+  .then(j => {
+
+    data = j.results.datetime[0].times;
+    console.log(data.Imsak);
+
+    document.querySelector('body')
+      .innerHTML = /*html*/ `<h1>${data.Maghrib}</h1>`
+  });
