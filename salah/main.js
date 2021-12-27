713cbac25e56dfd5c6d24d8796e3a2a301f9b179
saifuddien <ariefsaifuddien01@gmail.com>
2021-12-28 01:26:52 +0200

    lkl

--- salah/main.js
+++ salah/main.js
@@ -1,19 +1,25 @@
-fetch("https://api.aladhan.com/v1/timingsByCity?city=khartoum&country=sudan")
-  .then(m => m.json())
-  .then(j => {
+let cari = document.querySelector('.city');
+cari.addEventListener('keyup', async function (e) {
+  if (e.keyCode == '13') {
+    await fetch("https://api.aladhan.com/v1/timingsByCity?city=" + cari.value + "&country=country")
+      .then(m => m.json())
+      .then(j => {
 
-    data = j.data.timings;
+        data = j.data.timings;
 
-    document.querySelector('.content')
-      .innerHTML += /*html*/ `
-        <p>maghrib : ${data.Maghrib} </p>
-        <p>isha : ${data.Isha} </p>
-        <p>midnight : ${data.Midnight} </p>
-        <p>imsak : ${data.Imsak} </p>
-        <p>fajr : ${data.Fajr} </p>
-        <p>sunrise : ${data.Sunrise} </p>
-        <p>dhuhr : ${data.Dhuhr} </p>
-        <p>asr : ${data.Asr} </p>
-        <p>sunset : ${data.Sunset} </p>
-      `;
-  });
+        document.querySelector('.content')
+          .innerHTML = /*html*/ `
+              <h1>${cari.value}</h1>
+              <p>maghrib : ${data.Maghrib} </p>
+              <p>isha : ${data.Isha} </p>
+              <p>midnight : ${data.Midnight} </p>
+              <p>imsak : ${data.Imsak} </p>
+              <p>fajr : ${data.Fajr} </p>
+              <p>sunrise : ${data.Sunrise} </p>
+              <p>dhuhr : ${data.Dhuhr} </p>
+              <p>asr : ${data.Asr} </p>
+              <p>sunset : ${data.Sunset} </p>
+            `;
+      });
+  }
+});
