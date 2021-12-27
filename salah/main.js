52747e83bcc70fe6f2d64c444cd441fa0d486628
saifuddien <ariefsaifuddien01@gmail.com>
2021-12-28 00:35:53 +0200

    ada

--- salah/main.js
+++ salah/main.js
@@ -1,28 +1,19 @@
-fetch("./location.json")
+fetch("https://api.aladhan.com/v1/timingsByCity?city=khartoum&country=sudan")
   .then(m => m.json())
-  .then(a => {
+  .then(j => {
 
-    a.forEach(ad => {
+    data = j.data.timings;
 
-      fetch("https://api.pray.zone/v2/times/today.json?city=" + ad.capital)
-        .then(m => m.json())
-        .then(j => {
-
-          data = j.results.datetime[0].times;
-
-          document.querySelector('body')
-            .innerHTML += /*html*/ `
-              <h1>${ad.capital}</h1>
-              <p>maghrib : ${data.Maghrib} </p>
-              <p>isha : ${data.Isha} </p>
-              <p>midnight : ${data.Midnight} </p>
-              <p>imsak : ${data.Imsak} </p>
-              <p>fajr : ${data.Fajr} </p>
-              <p>sunrise : ${data.Sunrise} </p>
-              <p>dhuhr : ${data.Dhuhr} </p>
-              <p>asr : ${data.Asr} </p>
-              <p>sunset : ${data.Sunset} </p>
-            `;
-        });
-    });
+    document.querySelector('body')
+      .innerHTML += /*html*/ `
+        <p>maghrib : ${data.Maghrib} </p>
+        <p>isha : ${data.Isha} </p>
+        <p>midnight : ${data.Midnight} </p>
+        <p>imsak : ${data.Imsak} </p>
+        <p>fajr : ${data.Fajr} </p>
+        <p>sunrise : ${data.Sunrise} </p>
+        <p>dhuhr : ${data.Dhuhr} </p>
+        <p>asr : ${data.Asr} </p>
+        <p>sunset : ${data.Sunset} </p>
+      `;
   });
