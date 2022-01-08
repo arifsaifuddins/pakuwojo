563e343295fe085873456816c69125b39c8ef379
saifuddien <ariefsaifuddien01@gmail.com>
2022-01-08 23:40:57 +0200

    oke

--- salah/main.js
+++ salah/main.js
@@ -1,25 +1,39 @@
-let cari = document.querySelector('.city');
-cari.addEventListener('keyup', async function (e) {
-  if (e.keyCode == '13') {
-    await fetch("https://api.aladhan.com/v1/timingsByCity?city=" + cari.value + "&country=country")
-      .then(m => m.json())
-      .then(j => {
+let cari = document.querySelector( '#city' );
+cari.addEventListener( 'click', async function () {
+  await fetch( "https://api.aladhan.com/v1/timingsByCity?city=" + cari.value + "&country=country" )
+    .then( m => m.json() )
+    .then( j => {
 
-        data = j.data.timings;
+      let data = j.data.timings;
 
-        document.querySelector('.content')
-          .innerHTML = /*html*/ `
-              <h1>${cari.value}</h1>
-              <p>maghrib : ${data.Maghrib} </p>
-              <p>isha : ${data.Isha} </p>
-              <p>midnight : ${data.Midnight} </p>
-              <p>imsak : ${data.Imsak} </p>
-              <p>fajr : ${data.Fajr} </p>
-              <p>sunrise : ${data.Sunrise} </p>
-              <p>dhuhr : ${data.Dhuhr} </p>
-              <p>asr : ${data.Asr} </p>
-              <p>sunset : ${data.Sunset} </p>
-            `;
-      });
-  }
-});
+      document.querySelector( '.content' )
+        .innerHTML = /*html*/ `
+          <h1>${ cari.value }</h1>
+          <p>maghrib : ${ data.Maghrib } </p>
+          <p>isha : ${ data.Isha } </p>
+          <p>midnight : ${ data.Midnight } </p>
+          <p>imsak : ${ data.Imsak } </p>
+          <p>fajr : ${ data.Fajr } </p>
+          <p>sunrise : ${ data.Sunrise } </p>
+          <p>dhuhr : ${ data.Dhuhr } </p>
+          <p>asr : ${ data.Asr } </p>
+          <p>sunset : ${ data.Sunset } </p>
+        `;
+      console.log( parseInt( data.Asr ) );
+    } );
+
+  await fetch( 'http://api.weatherapi.com/v1/current.json?key=c9ce1b8c35d04ec4bfe171021220801&q=' + cari.value )
+    .then( w => w.json() ).then( j => {
+
+      let result = j.current.condition;
+
+      document.querySelector( '.weather' )
+        .innerHTML = /*html*/ `
+        <h1>${ j.location.name }</h1>
+        <img src="`+ result.icon + `" alt="">
+        <p>${ result.text }</p>
+        
+        `;
+
+    } );
+} );
